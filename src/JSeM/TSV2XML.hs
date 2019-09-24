4fcd976e699ede6e57ec8f8242d2e1b5993efad1
Daisuke Bekki <bekki@is.ocha.ac.jp>
2019-09-24 15:39:23 +0900

    Added Parsec parser for phenomena tag

--- src/JSeM/TSV2XML.hs
+++ src/JSeM/TSV2XML.hs
@@ -14,17 +14,21 @@ module JSeM.TSV2XML (
   checkTsvFile
   ) where
 
-import qualified Data.Map as M                   --container
-import qualified Data.Text as StrictT            --text
-import qualified Data.Text.Lazy as LazyT         --text
-import qualified Text.XML as X                   --xml-conduit
-import qualified Shelly as S                     --shelly
+import qualified Data.Map as M           --container
+import qualified Data.Text as StrictT    --text
+import qualified Data.Text.IO as StrictT    --text
+import qualified Data.Text.Lazy as LazyT --text
+import qualified Text.XML as X           --xml-conduit
+import qualified Shelly as S             --shelly
+import Text.Parsec as P                  --parsec
+import Text.Parsec.Text as P             --parsec
+--import Text.Parsec.Char as P             --parsec
 
 -- | tsv形式のJSeMデータをTextとして受け取り、
 -- | XML形式のJSeMデータを出力する。
 tsv2XML :: LazyT.Text -> LazyT.Text
 tsv2XML tsvlines  = 
-  nodes2XML "jsem-problems" $ map (tsvLine2xmlNode . (LazyT.split (=='\t'))) $ tail $ LazyT.lines tsvlines
+  nodes2XML "jsem-problems" $ map (tsvLine2xmlNode . (StrictT.split (=='\t')) . LazyT.toStrict) $ tail $ LazyT.lines tsvlines
 
 nkf :: FilePath -> IO(StrictT.Text)
 nkf filepath = S.shelly $ S.silently $ S.escaping False $ S.cmd $ S.fromText $ StrictT.concat ["cat '", StrictT.pack filepath, "'| nkf -w -Lu"]
@@ -63,7 +67,7 @@ nodes2XML tagname nodes =
 -}
 
 -- | TSV形式のJSeMテキストをJSeM式のXMLノードに変換
-tsvLine2xmlNode :: [LazyT.Text] -> X.Node
+tsvLine2xmlNode :: [StrictT.Text] -> X.Node
 tsvLine2xmlNode entry = 
   -- | entry!!0  1    
   -- | entry!!1  GQ間の関係：「ある」系-「すべて」系		
@@ -75,24 +79,20 @@ tsvLine2xmlNode entry =
   -- | entty!!7  P2 （空欄の場合はは<p idx="2">タグはなし）
   -- | entry!!8  Hすべての社員が異動を希望している。				      
   -- | entry!!9  note
-  let entry4 = entry!!4 -- JSeMのphenomena名はexcelのtsv変換で前後にダブルクオーテーションが付くので外す。
-      phenomena = if entry4 == ""
-                    then ""
-                    else let entry4_1 = if LazyT.head entry4 == '\"'
-                                          then LazyT.tail entry4
-                                          else entry4
-                             entry4_2 = if LazyT.last entry4_1 == '\"'
-                                          then LazyT.init entry4_1
-                                          else entry4_1 in
-                         LazyT.replace ", " "," entry4_2 in
-  X.NodeElement $ X.Element 
+  -- JSeMのphenomena名はexcelのtsv変換で前後にダブルクオーテーションが付くので外す。
+  let phenomena = case entry!!4 of
+                    "" -> ""
+                    e -> case parse phenomenaParser "" (entry!!4) of
+                           Left _ -> StrictT.concat ["parse error: ", entry!!4]
+                           Right p -> StrictT.intercalate "," p
+  in X.NodeElement $ X.Element 
                     (myname "problem")
                     (M.fromList 
-                       [("jsem_id",LazyT.toStrict (entry!!0)),
-                        ("answer",LazyT.toStrict (entry!!3)),
+                       [("jsem_id",entry!!0),
+                        ("answer",entry!!3),
                         ("language","ja"),
-                        ("phenomena",LazyT.toStrict phenomena),
-                        ("inference_type",LazyT.toStrict (entry!!5))
+                        ("phenomena",phenomena),
+                        ("inference_type",entry!!5)
                        ])
                     ([X.NodeElement $ X.Element 
                         (myname "link")
@@ -109,10 +109,10 @@ tsvLine2xmlNode entry =
                         [X.NodeElement $ X.Element
                            (myname "script")
                            (M.fromList [])
-                           [X.NodeContent (LazyT.toStrict (entry!!6))]                        
+                           [X.NodeContent (entry!!6)]                        
                         ]
                       ] ++  
-                      (if entry!!7 /= LazyT.empty
+                      (if entry!!7 /= StrictT.empty
                        then 
                          [X.NodeElement $ X.Element
                            (myname "p")
@@ -120,7 +120,7 @@ tsvLine2xmlNode entry =
                            [X.NodeElement $ X.Element
                              (myname "script")
                              (M.fromList [])
-                             [X.NodeContent (LazyT.toStrict (entry!!7))]
+                             [X.NodeContent (entry!!7)]
                            ]
                          ]  
                        else [])
@@ -131,11 +131,28 @@ tsvLine2xmlNode entry =
                         [X.NodeElement $ X.Element
                            (myname "script")
                            (M.fromList [])
-                           [X.NodeContent (LazyT.toStrict (entry!!8))]
+                           [X.NodeContent (entry!!8)]
                         ],
                       X.NodeElement $ X.Element
                         (myname "note")
                         (M.fromList [])
-                        [X.NodeContent (LazyT.toStrict (entry!!9))]
+                        [X.NodeContent (entry!!9)]
                       ])
 
+phenomenaParser :: Parser [StrictT.Text]
+phenomenaParser = do
+  char '\"'
+  phenomena <- sepBy (many1 $ noneOf ",\"") (string "," <|> string ", ")
+  char '\"'  
+  return $ map StrictT.pack phenomena
+
+-- test
+
+testp :: StrictT.Text
+testp = "\"Toritate, -sika (toritate particle), Negation\""
+  
+main :: IO()
+main = do
+  case parse phenomenaParser "" testp of
+    Left _ -> putStrLn "parse error"
+    Right p -> mapM_ StrictT.putStrLn p
