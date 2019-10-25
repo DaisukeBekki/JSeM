cd6d1ab5488b9c25263d7e4609d00fd5c477445b
Daisuke Bekki <bekki@is.ocha.ac.jp>
2019-10-25 20:26:47 +0900

    show-stat revised (to show the statistics of overall data)

--- app/show-stat.hs
+++ app/show-stat.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.ByteString.Lazy as B  --bytestring
-import qualified Data.List as L             --base
-import qualified Data.Map as M              --container
-import qualified Data.Text as T             --text
+import Control.Monad (forM)                 --base
+import System.Environment (getArgs)         --base
+import qualified System.Directory as D      --directory
+import System.FilePath ((</>),isExtensionOf) --filepath
+--import qualified Data.ByteString.Lazy as B --bytestring
 import qualified Data.Text.IO as T          --text
 import qualified Text.XML as X              --xml-conduit
 import qualified Text.XML.Cursor as X       --xml-conduit
+import qualified JSeM.XML as J              --jsem
 
 -- | JSeM統計情報出力プログラム
 -- |   標準入力からxmlフォーマットのJSeMファイルを受け取り、以下を標準出力に出力する：
@@ -15,23 +17,11 @@ import qualified Text.XML.Cursor as X       --xml-conduit
 -- | (3)<problem>要素内のphenomena属性の値のリストと出現数
 main :: IO()
 main = do
-  xml <- B.getContents
-  let cursor = X.fromDocument $ X.parseLBS_ X.def xml
-      problemNodes = X.child cursor >>= X.element "problem"
-      linkNodes = problemNodes >>= X.child >>= X.element "link"
-      --unexpectedcomments = problemNodes >>= X.child >>= X.checkNode isComment
-      transYes = linkNodes >>= X.laxAttribute "translation"
-      phenomena = problemNodes >>= X.laxAttribute "phenomena" >>= T.split (==',')
-      phen2 = L.map T.strip phenomena
-  --putStrLn "(0) Comments contained in <problem> tags:"
-  --mapM_ T.putStrLn unexpectedcomments
-  putStrLn $ "(1) The number of <problem> tags: "
-               ++ (show $ L.length $ problemNodes)
-               ++ "\n  which contains a <link> (to FraCaS dataset) tag: "
-               ++ (show $ L.length $ linkNodes)
-               ++ "\n(2) The number of yes in translation attr.: "
-               ++ (show $ L.length $ L.filter (=="yes") transYes)
-               ++ "\n(3) The number of occurrences of each phenomena:"
-  mapM_ T.putStrLn $ L.map (\(t,x) -> T.concat ["- ", t,": ",(T.pack $ show x)]) $ M.toList $ M.fromListWith (+) $ L.zip phen2 (repeat (1::Integer))
-
-
+  (dataFolder:_) <- getArgs
+  _ <- D.doesDirectoryExist dataFolder 
+  xmlFiles <- map (dataFolder </>) <$> filter (isExtensionOf "xml") <$> D.listDirectory dataFolder
+  stat <- J.problems2stat <$> concat <$> forM xmlFiles (\xmlFile -> do
+    cursor <- X.fromDocument <$> X.readFile X.def xmlFile 
+    return $ X.child cursor >>= X.element "problem"
+    )
+  T.putStrLn stat
--- src/JSeM/XML.hs
+++ src/JSeM/XML.hs
@@ -10,11 +10,15 @@ module JSeM.XML (
   JSeMData(..),
   JSeMLabel(..),
   xmlFile2JSeMData,
-  jsemData2Tsv
+  xmlFile2problems,
+  jsemData2Tsv,
+  problems2stat
   ) where
 
+import qualified Data.List as L           --base
+import qualified Data.Map as M            --container
 import qualified Data.Text as StrictT     --text
-import qualified Data.Text.IO as StrictT     --text
+import qualified Data.Text.IO as StrictT  --text
 import qualified Data.Text.Lazy as LazyT  --text
 import qualified Text.XML as X            --xml-conduit
 import qualified Text.XML.Cursor as X     --xml-conduit
@@ -48,11 +52,14 @@ jsemData2Tsv jsemdata =
       ] ++ (premises j) ++ [hypothesis j]
       )) jsemdata)
 
+xmlFile2problems :: FilePath -> IO([X.Cursor])
+xmlFile2problems xmlfile = do
+  cursor <- X.fromDocument <$> X.readFile X.def xmlfile
+  return $ X.child cursor >>= X.element "problem"
+
 -- | takes a file path of a JSeM file (XML format) and returns a list of 'JSeMData'.
 xmlFile2JSeMData :: FilePath -> IO([JSeMData])
-xmlFile2JSeMData filepath = do
-  cursor <- X.fromDocument <$> X.readFile X.def filepath
-  mapM problem2JSeMData $ X.child cursor >>= X.element "problem" 
+xmlFile2JSeMData filepath = xmlFile2problems filepath >>= mapM problem2JSeMData
 
 -- | takes a "problem" node in a JSeM file and translates it to a 'JSeMData'.  
 -- Note that the xml-conduit package uses Data.Text (=strict texts) as internal format of text data, 
@@ -74,3 +81,24 @@ problem2JSeMData problem = do
               "undef" -> return UNDEF
               _ -> fail $ StrictT.unpack $ StrictT.concat ["#", jsem_id, " has an undefined answer: ", answertext]
   return $ JSeMData jsem_id answer phenomena inference_type note premises hypothesis
+
+problems2stat :: [X.Cursor] -> StrictT.Text
+problems2stat [] = StrictT.empty
+problems2stat problems =
+  let linkNodes = problems >>= X.child >>= X.element "link"
+      --unexpectedcomments = problems >>= X.child >>= X.checkNode isComment
+      transYes = linkNodes >>= X.laxAttribute "translation"
+      phenomena = problems >>= X.laxAttribute "phenomena" >>= StrictT.split (==',')
+      phen2 = L.map StrictT.strip phenomena in
+  --putStrLn "(0) Comments contained in <problem> tags:"
+  --mapM_ StrictT.putStrLn unexpectedcomments
+  StrictT.append
+    (StrictT.pack $ "(1) The number of <problem> tags: "
+      ++ (show $ L.length $ problems)
+      ++ "\n  which contains a <link> (to FraCaS dataset) tag: "
+      ++ (show $ L.length $ linkNodes)
+      ++ "\n(2) The number of yes in translation attr.: "
+      ++ (show $ L.length $ L.filter (=="yes") transYes)
+      ++ "\n(3) The number of occurrences of each phenomena:\n")
+    (StrictT.intercalate "\n" $ L.map (\(t,x) -> StrictT.concat ["- ", t,": ",(StrictT.pack $ show x)]) $ M.toList $ M.fromListWith (+) $ L.zip phen2 (repeat (1::Integer)))
+
